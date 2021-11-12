8c8519a3c7f354b1d60d504fa1816ecf4f10e4b8
cverst <corstiaen.versteegh@gmail.com>
2021-11-12 01:57:07 +0100

    removed tfio dependency while preserving tensorflow optimization

--- dataloader.py
+++ dataloader.py
@@ -1,26 +1,27 @@
-import PIL
 import glob
 import os
 import tensorflow
-import tensorflow_io as tfio
 
 
 class DataLoader:
     """Class for creating tensorflow dataset."""
 
-    def __init__(self, path: str, batch_size: int = 32) -> None:
+    def __init__(self, path: str, batch_size: int = 32, n_samples: int = None) -> None:
         """Class instance initialization.
 
         Args:
             path (str): Path to data folder with "map" and "mask" pairs.
             batch_size (int, optional): Batch size for model training.
                 Defaults to 32.
+            n_samples(int, optional): Number of input-target pairs to load.
+                Returns all available pairs if set to None. Defaults to None.
         """
         # initialize attributes
         self.path = path
         self.batch_size = batch_size
-        self.dataset_input = None
-        self.dataset_target = None
+        self.n_samples = n_samples
+        self._dataset_input = None
+        self._dataset_target = None
         self.dataset = None
 
         # initialize self.dataset_input and self.dataset_target
@@ -34,8 +35,8 @@ class DataLoader:
         img_paths, target_paths = self._get_img_paths()
 
         # create datasets
-        self.dataset_input = tensorflow.data.Dataset.from_tensor_slices(img_paths)
-        self.dataset_target = tensorflow.data.Dataset.from_tensor_slices(target_paths)
+        self._dataset_input = tensorflow.data.Dataset.from_tensor_slices(img_paths)
+        self._dataset_target = tensorflow.data.Dataset.from_tensor_slices(target_paths)
 
     def _get_img_paths(self) -> tuple:
         """Retrieves all image paths for input and targets present in
@@ -51,15 +52,26 @@ class DataLoader:
         all_paths = glob.glob(os.path.join(self.path, "*.tif"))
 
         # data has not been curated and wrongly sized images are discarded
-        good_paths = self._discard_wrong_img_paths(all_paths)
+        useable_paths = self._discard_wrong_img_paths(all_paths)
+        useable_paths.sort()
+
+        # keep only part of image paths if n_samples was specified
+        if self.n_samples is None:
+            self.n_samples = len(useable_paths)
+
+        assert self.n_samples <= len(
+            useable_paths
+        ), f"n_samples ({self.n_samples}) is greater than number of available/useable images."
+        useable_paths = useable_paths[: self.n_samples]
 
         # split input and target
-        input_paths = [filename for filename in good_paths if "map" in filename]
-        target_paths = [filename for filename in good_paths if "mask" in filename]
+        input_paths = [filename for filename in useable_paths if "map" in filename]
+        target_paths = [filename for filename in useable_paths if "mask" in filename]
 
-        # sort
-        input_paths.sort()
-        target_paths.sort()
+        assert len(input_paths) == len(
+            target_paths
+        ), f"Number of input images ({len(input_paths)}) does not match\
+                 number of target images ({len(target_paths)})."
 
         return input_paths, target_paths
 
@@ -67,39 +79,46 @@ class DataLoader:
         """Discard wrong files; function is temporary."""
         correct_filenames = []
         for path in all_paths:
-            if PIL.Image.open(path).size == (224, 224):
+            if tensorflow.keras.utils.load_img(path).size == (224, 224):
                 correct_filenames.append(path)
         return correct_filenames
 
-    def _load_image(self, path: str, is_input: bool) -> tensorflow.image:
+    def _load_image(self, tensor: tensorflow.Tensor, channels: str) -> tensorflow.image:
         """Load .tif image as tensor and perform normalization.
 
         Args:
-            path (str): File name path of .tif image to be loaded.
-            is_input (bool): If True the output image keeps its RGB color
-                channels and is then normalized to 255=1.0. If False only the
-                alpha channel of the RGBA color mode is returned without
-                further normalization.
+            tensor (tensorflow.Tensor): Tensor with file name path of .tif
+                image to be loaded.
+            channels (str): The color channels of the image that is desired.
+                Either "RGB" or "A" (alpha). All channels are normalized such
+                that 255 = 1.0.
 
         Returns:
             tensorflow.image: An image tensor of type float32 normalized from
                 0-255 to 0.0-1.0.
         """
-        # read file and decode
-        img_file = tensorflow.io.read_file(path)
-        img_rgba = tfio.experimental.image.decode_tiff(img_file)
+        # decode tensor and read image using helper function
+        def _decode_tensor_load_image(tensor, color_mode):
+            img = tensorflow.keras.utils.load_img(
+                tensor.numpy().decode("utf-8"),
+                color_mode=color_mode,
+            )
+            return img
 
-        # convert to float32
-        converted_img = tensorflow.image.convert_image_dtype(
-            img_rgba, tensorflow.float32
+        [img_rgba,] = tensorflow.py_function(
+            _decode_tensor_load_image, [tensor, "rgba"], [tensorflow.float32]
         )
 
-        # keep the right color channels
-        if is_input:
-            img = converted_img[:, :, :3] / 255.0
+        # normalize and keep queried channels
+        img_rgba = tensorflow.math.divide(img_rgba, 255.0)
+        if channels == "RGB":
+            img = img_rgba[:, :, :3]
+        elif channels == "A":
+            img = tensorflow.math.ceil(img_rgba[:, :, 3])
+        # TODO: Double check the line above is correct. Why are there
+        #       non-zero-one values in the alpha channel?
         else:
-            img = converted_img[:, :, 3]
-
+            raise ValueError("Unkown channels specified. Use 'RGB' or 'A'.")
         return img
 
     def load(self, buffer_size: int = None) -> None:
@@ -119,15 +138,15 @@ class DataLoader:
         """
         # if unset, set buffersize to number of samples
         if not buffer_size:
-            buffer_size = self.dataset_input.cardinality().numpy()
+            buffer_size = self._dataset_input.cardinality().numpy()
 
         # load images for inputs and targets
-        inputs = self.dataset_input.map(
-            lambda p: self._load_image(path=p, is_input=True),
+        inputs = self._dataset_input.map(
+            lambda t: self._load_image(tensor=t, channels="RGB"),
             num_parallel_calls=tensorflow.data.experimental.AUTOTUNE,
         )
-        targets = self.dataset_target.map(
-            lambda p: self._load_image(path=p, is_input=False),
+        targets = self._dataset_target.map(
+            lambda t: self._load_image(tensor=t, channels="A"),
             num_parallel_calls=tensorflow.data.experimental.AUTOTUNE,
         )
 
