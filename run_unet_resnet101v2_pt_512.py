8465e0e60f7580e32f89cbe35a7b0521ad8adb3c
Jeremie Joudioux <j.joudioux@icloud.com>
2021-12-04 11:42:18 +0100

    Updated run unet resnet with the correct call for the model

--- run_unet_resnet101v2_pt_512.py
+++ run_unet_resnet101v2_pt_512.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 import tensorflow
-from model import Model
+from models.unet_resnet101v2_pt import Model
 
 PATH_TRAIN = "data/small_large/train"
 PATH_VAL = "data/small_large/test"
