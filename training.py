305160d689cc1b2a497d63da523a9d449b2fcfd0
Jeremie Joudioux <j.joudioux@icloud.com>
2021-12-05 15:52:32 +0100

    updated model class (layer included), added logging class, updated the training. Explanation on how to use pickle model is added

--- training.py
+++ training.py
@@ -1,18 +1,14 @@
 # For saving models, refere to
 # https://www.tensorflow.org/guide/keras/save_and_serialize
 import tensorflow
-from class_unet_resnet101v2 import Unet
+
 from roof.dataloader import DataLoader
+from class_unet_resnet101v2 import Unet
+from logging_class import Logs
 
 
 # Layers for the skip connections.
-layer_names = [
-    "conv1_conv",
-    "conv2_block2_out",
-    "conv3_block3_out",
-    "conv4_block22_out",
-    "conv5_block2_out",
-]
+
 # parameters of the model.
 output_classes = 1  # number of categorical classes.
 input_shape = (512, 512, 3)  # input size
@@ -24,7 +20,6 @@ path_test = "data/small_large/test"
 
 # calling the model.
 model = Unet(
-    layer_names=layer_names,
     output_classes=output_classes,
     drop_out=True,
     drop_out_rate={"512": 0.25, "256": 0.3, "128": 0.35, "64": 0.4},
@@ -38,17 +33,49 @@ dl_train = DataLoader(
     batch_size=batch_size,
     input_shape=input_shape,
 )
-dl_val = DataLoader(
+dl_test = DataLoader(
     path_test,
     batch_size=batch_size,
     input_shape=input_shape,
 )
 dl_train.load()
-dl_val.load()
-
+dl_test.load()
 
 train_batches = dl_train.dataset
-test_batches = dl_val.dataset
+test_batches = dl_test.dataset
+
+# Starting the logs
+
+log = Logs()
+comment = "This is a smart comment"
+log.comment(comment=comment)
+
+
+# Preparing the model to be saved using a checkpoint
+
+model_checkpoint_callback = tensorflow.keras.callbacks.ModelCheckpoint(
+    filepath=log.checkpoint_filepath,
+    save_weights_only=False,
+    monitor="val_accuracy",
+    mode="max",
+    save_best_only=True,
+    verbose=1,
+)
+
+# Prepare the tensorboard
+
+tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(
+    log_dir=log.tensorboard_path,
+    histogram_freq=1,
+    write_graph=True,
+)
+
+patience = 10
+# Parameters for early stopping
+early_stopping = tensorflow.keras.callbacks.EarlyStopping(
+    monitor="val_loss",
+    patience=patience,
+)
 
 
 # compiling the model
@@ -69,22 +96,36 @@ model.compile(
 # training the model.
 epochs = 1
 steps_per_epoch = dl_train.n_samples / batch_size
-validation_steps = max(dl_val.n_samples // batch_size, 1)
+validation_steps = max(dl_test.n_samples // batch_size, 1)
 
 history = model.fit(
     train_batches,
     epochs=epochs,
     steps_per_epoch=steps_per_epoch,
     validation_steps=validation_steps,
+    validation_data=test_batches,
+    callbacks=[
+        model_checkpoint_callback,
+        tensorboard_callback,
+        early_stopping,
+    ],
 )
 
+# I am explaining how to load the model's weight for a custom model.
+# First you get the overwritten configuration of the custim model.
 model_dict = model.get_config()
 
-print(model_dict)
+# Second you create the model from the configuration dictionary. This creates a new models with the same layer configuration
+new_model = model.from_config(model_dict)
+# Finally, you load the weights in the new models.
+new_model.load_weights(log.checkpoint_filepath)
 
-# saving the model
 
-tensorflow.saved_model.save(model, "mymodel")
+# logging examples of prediction on the test data sets.
+num_batches = 4  # number of batches used to display sample predictions.
 
-# Loading model weights. Note that the full model cannot be loaded for subclassed model
-loaded_1 = tensorflow.saved_model.load("mymodel")
+log.show_predictions(
+    dataset=dl_test.dataset,
+    model=new_model,
+    num_batches=num_batches,
+)
