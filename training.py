8a0b41268023352c227fbdbcf9b68e60a9e12f68
Jeremie Joudioux <j.joudioux@icloud.com>
2021-12-06 11:28:32 +0100

    updated log class, updated training file

--- training.py
+++ training.py
@@ -6,26 +6,23 @@ from roof.dataloader import DataLoader
 from class_unet_resnet101v2 import Unet
 from logging_class import Logs
 
-
-# Layers for the skip connections.
+tensorflow.keras.backend.clear_session()
 
 # parameters of the model.
 output_classes = 1  # number of categorical classes.
 input_shape = (512, 512, 3)  # input size
 batch_size = 4  # batchsize
 # Path to the data
-path_train = "data/large/train"
-path_test = "data/large/test"
+path_train = "data/small_large/train"
+path_test = "data/small_large/test"
 
 
 # calling the model.
 model = Unet(
     output_classes=output_classes,
     drop_out=True,
-    drop_out_rate={"512": 0.25, "256": 0.3, "128": 0.35, "64": 0.4},
-    fine_tune_at=20,
+    drop_out_rate={"512": 0.2, "256": 0.25, "128": 0.3, "64": 0.35},
 )
-
 
 # Loading the data
 dl_train = DataLoader(
@@ -33,6 +30,7 @@ dl_train = DataLoader(
     batch_size=batch_size,
     input_shape=input_shape,
 )
+
 dl_test = DataLoader(
     path_test,
     batch_size=batch_size,
@@ -46,11 +44,18 @@ test_batches = dl_test.dataset
 
 # Starting the logs
 
-log = Logs(model=model)
+log = Logs()
 comment = "Full large dataset, increasing dropping rate in upstack,\n\
      new version of the model in its class form. "
-log.comment(comment=comment)
+log.main_log(
+    comment=comment,
+    model_config=model.get_config(),
+)
 
+log.local_log(
+    train_data_config=dl_train.get_config(),
+    val_data_config=dl_test.get_config(),
+)
 
 # Preparing the model to be saved using a checkpoint
 
@@ -117,15 +122,16 @@ history = model.fit(
 model_dict = model.get_config()
 
 # Second you create the model from the configuration dictionary. This creates a new models with the same layer configuration
-new_model = model.from_config(model_dict)
+best_model = model.from_config(model_dict)
 # Finally, you load the weights in the new models.
-new_model.load_weights(log.checkpoint_filepath)
+best_model.load_weights(log.checkpoint_filepath)
 
 
 # logging examples of prediction on the test data sets.
-num_batches = 4  # number of batches used to display sample predictions.
+num_batches = 20  # number of batches used to display sample predictions.
 
 log.show_predictions(
     dataset=dl_test.dataset,
+    model=best_model,
     num_batches=num_batches,
 )
