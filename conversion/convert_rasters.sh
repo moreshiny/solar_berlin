620ed811d1c91d8c0c60ef5fe04ac7b51d82a52b
Daniel Bumke <post@danielbumke.de>
2021-12-09 13:08:40 +0100

    Improve efficiency of download and conversion

--- conversion/convert_rasters.sh
+++ conversion/convert_rasters.sh
@@ -16,34 +16,39 @@ mkdir -pv $CONVERTED_FOLDER
 # for each file in the raw folder
 for zipfile in $RAW_RASTER_FOLDER/*.zip
 do
-    # extract the zip file
-    echo "Extracting" $zipfile
-    unzip $zipfile -d $TEMPORARY_FOLDER
-    echo "..done. $zipfile"
     region=$(basename $zipfile .zip)
-    # for each raster file in the temp folder
-    for raster_file in $TEMPORARY_FOLDER/*.ecw
+    echo "Converting $region"
+
+    # get the list of files in the zip file
+    unzip -l $zipfile | awk '{print $4}' | grep ".ecw$" > $TEMPORARY_FOLDER/filelist.txt
+
+    for raster_file in $(cat $TEMPORARY_FOLDER/filelist.txt)
     do
-        echo "Converting $raster_file"
         # remove the extension from the raster file name
         output_filename=$(basename $raster_file .ecw)
 
         #if the output file doesn't exist
         if [ ! -f $CONVERTED_FOLDER/$region-$output_filename.tif ]
         then
+            echo "Converting $raster_file"
+            unzip -n $zipfile -d $TEMPORARY_FOLDER $raster_file
+            unzip -n $zipfile -d $TEMPORARY_FOLDER $output_filename.eww
+
             # convert the ecw to tif - this requires that gdal has been built
             # with ecw support
             gdal_translate -of GTiff \
-                $raster_file $CONVERTED_FOLDER/$region-$output_filename.tif \
+                $TEMPORARY_FOLDER/$raster_file $CONVERTED_FOLDER/$region-$output_filename.tif \
                 -co COMPRESS=DEFLATE -co NUM_THREADS=ALL_CPUS
+
+            # remove the temporary raster file
+            rm $TEMPORARY_FOLDER/$raster_file
+            rm $TEMPORARY_FOLDER/$output_filename.eww
+            echo "...done $raster_file"
         else
             echo "Skipping $raster_file as output file already exists"
         fi
-        # remove the temporary raster file
-        rm $raster_file
-        rm $TEMPORARY_FOLDER/$output_filename.eww
-        echo "...done $raster_file"
     done
+    rm $TEMPORARY_FOLDER/filelist.txt
 done
 
 # remove the temp folder
