b3153129609648dee45cdd7fbcaad5a9b6ff509f
Jeremie Joudioux <j.joudioux@icloud.com>
2021-12-11 15:27:31 +0100

    tinder like data cleaning added, as well as a runf file added

--- test_cleaning_class.py
+++ test_cleaning_class.py
@@ -1,55 +1,65 @@
 from class_automated_data_cleaning import DataCleaning
 from class_unet_resnet101v2 import Unet
 
-
-
-
+# Define the parameters of the model to be used.
 output_classes = 5  # number of categorical classes.
 input_shape = (512, 512, 3)  # input size
 
-#accuracy threshild for the cleaning
 
-# calling the model.
+# calling the model. For inference, drop out must be deactivated.
+# The structure of the model must be identical to the loaded model.
 model = Unet(
     output_classes=output_classes,
     input_shape=input_shape,
     drop_out=False,
-    drop_out_rate={"512": 0.275, "256": 0.3, "128": 0.325, "64": 0.35},
     multiclass=bool(output_classes - 1),
 )
 
+# Path to check point. This is a directory. Lead to an error when calling it.
+PATH_CHECKPOINT = "Logs/12_09_2021_22_25_55/checkpoint.ckpt"
 
-path_checkpoint = "logs/12_09_2021_22_25_55/checkpoint.ckpt"
+# Loading the weights
+model.load_weights(PATH_CHECKPOINT)
 
-model.load_weights(path_checkpoint)
+# Define the path of the folder containing the images to sort
+PATH_TO_CLEAN = "data/selected_512_multiclass/selected_tiles_512_100_20_42/train"
 
-path_to_clean = "data/selected_tiles_512_4000_1000_42_cleaned/train"
 
+# call the cleaning class.
 cleaning = DataCleaning(
-        path_to_clean = path_to_clean,
-        input_shape = (512, 512, 3),
-        model = model,
+    path_to_clean=PATH_TO_CLEAN,
+    input_shape=(512, 512, 3),
+    model=model,
 )
 
-cleaning.cleaning(proportion=0.05)
+# Perform the cleaning. The proportion parameter signals the proportion of samples to study.
+# These samples are copied in a folder called dirty by default.
+# if the folder already exists, an exception will be raised.
+# To delete, use the parameters of the _move_bad_files method of the class.
+PROPORTION = 0.1
+cleaning.cleaning(proportion=PROPORTION, out_folder_name="dirty")
 
 
 print("Done")
 
 
+# Calling the manual sorting. If the cleaning method of the class has been called before,
+#the sorting starts with the picture with the highest loss. The content is loaded from the Dataframe
+# cleaning._discard_df. if not, define the classe, and
+# specifiy here te path to clean by setting class_called = False, and
+#path_to_clean = "your_path". Return the list of paths manually discarded.
+discard_list = cleaning.manual_sorting(class_called=True)
+#Stand-alone usage
+#discard_list = cleaning.manual_sorting(class_called=False, path_to_clean = PATH_TO_CLEAN)
 
-path_to_clean = "data/selected_tiles_512_4000_1000_42_cleaned/test"
 
-cleaning = DataCleaning(
-        path_to_clean = path_to_clean,
-        input_shape = (512, 512, 3),
-        model = model,
-)
-
-cleaning.cleaning(proportion=0.05)
+#Print the list of the paths manually discared into a folder to save it.
+with open(
+    PATH_TO_CLEAN + "/discarded.txt", "a", encoding="utf-8"
+) as discarded_elements:
+    for path in discard_list:
+        discarded_elements.write(path)
+        discarded_elements.write("\n")
 
 
 print("Done")
-
-
-
