efdb7df57d460993e50e91e2dcb9fff3b0f9c24d
Jeremie Joudioux <j.joudioux@icloud.com>
2021-12-13 14:14:01 +0100

    updated requirements with pandas

--- training.py
+++ training.py
@@ -10,44 +10,46 @@ from logging_class import Logs
 tensorflow.keras.backend.clear_session()
 
 # parameters of the model.
-output_classes = 5  # number of categorical classes. for 2 classes = 1.
-input_shape = (512, 512, 3)  # input size
-epochs = 35
+OUTPUT_CLASSES = 5  # number of categorical classes. for 2 classes = 1.
+INPUT_SHAPE = (512, 512, 3)  # input size
+EPOCHS = 35
 
-batch_size = 16  # batchsize
+BATCH_SIZE = 8  # batchsize
 # Path to the data large multiclass dataset
 # path_train = "data/selected_tiles_512_4000_1000_42_partial/train"
 # path_test = "data/selected_tiles_512_4000_1000_42_partial/test"
 
 # Path to the data small multiclass dataset
-#path_train = "data/selected_512_multiclass/selected_tiles_512_100_20_42/train"
-#path_test = "data/selected_512_multiclass/selected_tiles_512_100_20_42/test"
+# path_train = "data/selected_512_multiclass/selected_tiles_512_100_20_42/train"
+# path_test = "data/selected_512_multiclass/selected_tiles_512_100_20_42/test"
 
 # path to the small mono class large dataset
 # path_train = "data/small_large/train"
 # path_test = "data/small_large/test"
 
-#Path to data for Daniel local machine: Half dataset
-path_train="data/selected_tiles_512_4000_1000_42_cleaned/train"
-path_test="data/selected_tiles_512_4000_1000_42_cleaned/test"
+# Path to data for Daniel local machine: Half dataset
+PATH_TRAIN = "data/cleaned_4000_extract/train"
+PATH_TEST = "data/cleaned_4000_extract/test"
 
 
 # calling the model.
 model = Unet(
-    output_classes=output_classes,
-    input_shape=input_shape,
+    output_classes=OUTPUT_CLASSES,
+    input_shape=INPUT_SHAPE,
     drop_out=True,
     drop_out_rate={"512": 0.3, "256": 0.35, "128": 0.4, "64": 0.45},
-    multiclass=bool(output_classes - 1),
+    multiclass=bool(OUTPUT_CLASSES - 1),
 )
 
 # Starting the logs
 
 log = Logs()
-comment = "Full large dataset, multiclassification problem ,\n\
-     standard learning rate.  "
+COMMENT = "Full large dataset, with the 20pc highst loss cleaned with the \n\
+    with the latest model, multiclassification problem ,\n\
+    standard learning rate."
+
 log.main_log(
-    comment=comment,
+    comment=COMMENT,
     model_config=model.get_config(),
 )
 
@@ -64,8 +66,8 @@ sparse_categorical_accuracy = tensorflow.keras.metrics.SparseCategoricalAccuracy
 recall = tensorflow.keras.metrics.Recall(name="recall")
 precision = tensorflow.keras.metrics.Precision(name="precision")
 
-if output_classes > 1:
-    multiclass = True
+if OUTPUT_CLASSES > 1:
+    MULTICLASS = True
     loss = tensorflow.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
     metric_list = [
         "sparse_categorical_accuracy",
@@ -80,7 +82,7 @@ if output_classes > 1:
         verbose=1,
     )
 else:
-    multiclass = False
+    MULTICLASS = False
     loss = tensorflow.keras.losses.BinaryCrossentropy(from_logits=False)
     metric_list = [
         "accuracy",
@@ -98,25 +100,25 @@ else:
     )
 
 dl_train = DataLoader(
-    path_train,
-    batch_size=batch_size,
-    input_shape=input_shape,
+    PATH_TRAIN,
+    batch_size=BATCH_SIZE,
+    input_shape=INPUT_SHAPE,
     legacy_mode=False,
-    multiclass=multiclass,
+    multiclass=MULTICLASS,
 )
 
 
 dl_test = DataLoader(
-    path_test,
-    batch_size=batch_size,
-    input_shape=input_shape,
+    PATH_TEST,
+    batch_size=BATCH_SIZE,
+    input_shape=INPUT_SHAPE,
     legacy_mode=False,
-    multiclass=multiclass,
+    multiclass=MULTICLASS,
 )
 
 
-dl_train.load(buffer_size = 500)
-dl_test.load(shuffle = False)
+dl_train.load(buffer_size=500)
+dl_test.load(shuffle=False)
 
 train_batches = dl_train.dataset
 test_batches = dl_test.dataset
@@ -136,19 +138,19 @@ tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(
     write_graph=True,
 )
 
-patience = 7
+PATIENCE = 7
 # Parameters for early stopping
 early_stopping = tensorflow.keras.callbacks.EarlyStopping(
     monitor="val_loss",
-    patience=patience,
+    patience=PATIENCE,
 )
 
 
 print("callbacks defined")
 
 # compiling the model
-learning_rate = 0.001
-opt = tensorflow.keras.optimizers.Adam(learning_rate=learning_rate)
+LEARNING_RATE = 0.001
+opt = tensorflow.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
 
 
 model.compile(optimizer=opt, loss=loss, metrics=metrics)
@@ -157,13 +159,13 @@ model.compile(optimizer=opt, loss=loss, metrics=metrics)
 print("compiling done")
 # training the model.
 
-steps_per_epoch = dl_train.n_samples // batch_size
-validation_steps = max(dl_test.n_samples // batch_size, 1)
+steps_per_epoch = dl_train.n_samples // BATCH_SIZE
+validation_steps = max(dl_test.n_samples // BATCH_SIZE, 1)
 
 
 history = model.fit(
     train_batches,
-    epochs=epochs,
+    epochs=EPOCHS,
     steps_per_epoch=steps_per_epoch,
     validation_steps=validation_steps,
     validation_data=test_batches,
@@ -188,12 +190,12 @@ log.local_log(
     metrics=accuracies,
 )
 
-num_batches = 5  # number of batches
+NUM_BATCHES = 5  # number of batches
 log.show_predictions(
     dataset=dl_test.dataset,
     model=model,
-    num_batches=num_batches,
-    multiclass=multiclass,
+    num_batches=NUM_BATCHES,
+    multiclass=MULTICLASS,
 )
 
 # print("first fitting round")
@@ -205,7 +207,7 @@ log.show_predictions(
 # model_dict["fine_tune_at"] = 4
 # model_dict["upstack_trainable"] = True
 
-# # Second you create the model from the configuration dictionary. This creates a new models with the same layer configuration
+#  Second you create the model from the configuration dictionary. This creates a new models with the same layer configuration
 # best_model = Unet.from_config(model_dict)
 # # Finally, you load the weights in the new models.
 # # best_model.load_weights(log.checkpoint_filepath)
