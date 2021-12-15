4027f893610ebfab7dedbec39f9224cc816ea761
Jeremie Joudioux <j.joudioux@icloud.com>
2021-12-15 17:08:30 +0100

    test cleaning file to run the test cleaning class without automated cleaning

---
+++ test_cleaning_no_model.py
@@ -0,0 +1,30 @@
+# Data cleaning in the absence of
+
+from class_automated_data_cleaning import DataCleaning
+
+
+PATH_TO_CLEAN = "data/bin_clean_4000/test"
+
+
+# call the cleaning class.
+cleaning = DataCleaning(
+    path_to_clean=PATH_TO_CLEAN,
+    input_shape=(512, 512, 3),
+)
+
+discard_list = cleaning.manual_sorting()
+
+
+print("Manual sorting Done")
+
+
+list_discarded = cleaning.move_discarded_files(
+    output_folder_name="dirty",
+    delete_existing_output_path_no_warning=True,
+)
+
+print(list_discarded)
+print("File moved.")
+
+
+print("Done")
